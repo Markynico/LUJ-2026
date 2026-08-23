aba175770fbd2fcb5bb3a9e33a52f669ea5110c8
attietesta <attietesta@gmail.com>
2026-08-23 18:26:08 -0300

    selector de niveles

---
+++ luj-2026/scripts/niveles/tipo_de_sala.gd
@@ -0,0 +1,9 @@
+class_name TipoDeSala
+
+enum Tipo { NORMAL, TIENDA, LOOT }
+
+const COLORES := {
+	Tipo.NORMAL: Color(0.4, 0.7, 1.0, 0.5),
+	Tipo.TIENDA: Color(1.0, 0.85, 0.3, 0.5),
+	Tipo.LOOT: Color(0.8, 0.4, 1.0, 0.5),
+}
