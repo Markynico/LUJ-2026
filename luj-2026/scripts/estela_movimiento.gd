23cbda3fb42841f7259657bd1ee468a6e598e5d2
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2026-08-22 23:11:52 -0300

    estela movimiento, trayectoria y base de resources para pelotitas

---
+++ luj-2026/scripts/estela_movimiento.gd
@@ -0,0 +1,21 @@
+class_name EstelaMovimiento
+extends Line2D
+
+##distancia maxima de la estela de movimiento, a mayor valor mas larga es la estela
+@export var distancia_maxima: float = 25.0
+@export var body: BolaDePelos
+ 
+func _ready() -> void:
+	set_as_top_level(true)
+ 
+func set_color_estela(gradiente : Gradient):
+	gradient = gradiente
+ 
+func _physics_process(_delta: float) -> void:
+	if not body:
+		return
+ 
+	add_point(body.global_position)
+ 
+	if points.size() > distancia_maxima:
+		remove_point(0)
