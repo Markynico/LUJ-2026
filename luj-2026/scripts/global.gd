5ec449216e51efe800384b7f3d7204a2c6feb0f7
attietesta <attietesta@gmail.com>
2026-08-24 00:32:10 -0300

    monedas y puntos global

---
+++ luj-2026/scripts/global.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var monedas : int = 0
+var puntos : int = 0
+
+signal monedas_cambiadas(monedas : int)
+signal puntos_cambiados(puntos : int)
+
+func actualizar_monedas(valor):
+	monedas += valor
+	monedas_cambiadas.emit(monedas)
+
+func actualizar_puntos(valor):
+	puntos += valor
+	puntos_cambiados.emit(puntos)
