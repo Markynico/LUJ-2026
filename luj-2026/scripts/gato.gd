11afd580f5529fb2ab7892b54144b361ac94bf39
WaterLiliths <waterliliths@gmail.com>
2026-08-23 22:09:42 -0300

    Lanzar gato
    El gato ya puede ser lanzado
    - Se creó la escena/clase Gato
    - DisparadorPelotitas ahora es hijo de Gato

---
+++ luj-2026/scripts/gato.gd
@@ -0,0 +1,54 @@
+class_name Gato
+extends RigidBody2D
+
+signal escupir_bola
+
+@export var disparador_pelotitas : DisparadorPelotita
+@export var game_manager : GameManager
+@export var colision : CollisionShape2D
+
+@export var fuerza_disparo : float = 2.0
+
+var velocidad_inicial : Vector2
+var posicion_mouse : Vector2
+var listo_para_lanzar : bool
+
+func _ready() -> void:
+	freeze = true
+	colision.set_deferred("disabled", true)
+	listo_para_lanzar = false
+	game_manager.gato_lanza_bola.connect(preparar_bola)
+	game_manager.lanzar_gato.connect(preparar_lanzamiento)
+
+#============ INPUT ==============
+func _input(event: InputEvent) -> void:
+	if not listo_para_lanzar:
+		return
+	
+	if event is InputEventMouseMotion: #Se esta moviendo el mouse
+		posicion_mouse = get_global_mouse_position()
+		velocidad_inicial = (global_position - posicion_mouse) * fuerza_disparo #no lo normalizo para q justamente dispare mas fuerte si el mouse esta lejos
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			lanzar()
+
+#============ FUNCIONES ==============
+
+func preparar_lanzamiento():
+	call_deferred("habilitar_lanzamiento")
+
+
+func habilitar_lanzamiento():
+	listo_para_lanzar = true
+	print("GATO LANZAMIENTO LISTO")
+
+func lanzar():
+	freeze = false
+	colision.set_deferred("disabled", false)
+	apply_impulse(-velocidad_inicial)
+	
+	listo_para_lanzar = false
+
+
+func preparar_bola():
+	disparador_pelotitas.escupir_bola()
