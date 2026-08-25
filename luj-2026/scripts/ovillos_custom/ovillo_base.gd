88d330c051770d36e57204108bb6121acd9b9e0e
WaterLiliths <waterliliths@gmail.com>
2026-08-25 10:45:45 -0300

    Sistema de probabilidad de ovillos
    - Se agregó un sistetema que elige ovillos en función de la probabilidad de salir que tienen
    - Se agregó la variable puntos en el recurso OvilloBase
    - Se agregó la variable valor_spawn en el recurso OvilloBase
    - Se agergaron y editaron ovillos

--- luj-2026/scripts/ovillos_custom/ovillo_base.gd
+++ luj-2026/scripts/ovillos_custom/ovillo_base.gd
@@ -8,5 +8,7 @@ extends Resource
 @export var cant_monedas : int = 1
 ##a la bola de pelos se le va a sumar este rebote extra, si no queremos q tenga rebote extra lo dejamos en cero y listo el posho (:
 @export var rebote_extra : float = 10
+@export var puntaje : int  = 1
+@export var valor_spawn : float = 10.0
 @export var color_numeros_impacto : Color
 @export var efectos_al_recibir_impacto : Array[EfectosOvillo]
