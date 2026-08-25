ff62218a4c091698daa953da5473371b9ebf197b
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2026-08-24 21:35:40 -0300

    Revert "Merge pull request #22 from Markynico/marcos2"
    This reverts commit fc2d1439eca92522423afd2ad89980e40fe5a66a, reversing
    changes made to 72844f9a66b6a7db968a8600c1426d6acef83f56.

--- luj-2026/scripts/ovillo.gd
+++ luj-2026/scripts/ovillo.gd
@@ -9,24 +9,20 @@ extends StaticBody2D
 @export var forma_colision : CollisionShape2D
 @export var sprite_normal : Sprite2D
 @export var sprite_desactivado : Sprite2D
-@export var pergamino_info : PergaminoInfo
-@export var numero_impacto : NumeroImpacto
+
 var activado : bool = true
 
 func _ready() -> void:
 	sprite_desactivado.hide()
 	audio.stream = tipo_ovillo.audio
 	sprite_normal.texture = tipo_ovillo.sprite
-	pergamino_info.set_texto_ovillo(tipo_ovillo)
 
 func recibir_impacto() -> void: #se llama desde la bola de pelos al impactar con esto
 	if not activado:
 		return
 	desactivar_ovillo()
-	#numero_impacto.iniciar_numero_impacto(tipo_ovillo.cant_monedas)
 	for efecto in tipo_ovillo.efectos_al_recibir_impacto:
 		efecto.al_recibir_impacto(self)
-		#emitir dar monedas tipo_ovillo.cantidadmondedas
 		pass
 
 func desactivar_ovillo():
--- luj-2026/scripts/ovillos_custom/ovillo_base.gd
+++ luj-2026/scripts/ovillos_custom/ovillo_base.gd
@@ -1,12 +1,6 @@
 class_name OvilloBase
 extends Resource
 
-@export var nombre : String = ""
 @export var sprite : Texture
 @export var audio : AudioStream
-##cantidad de monedas q obtendra el jugador cuando le peguemos a este ovillo
-@export var cant_monedas : int = 1
-##a la bola de pelos se le va a sumar este rebote extra, si no queremos q tenga rebote extra lo dejamos en cero y listo el posho (:
-@export var rebote_extra : float = 10
-@export var color_numeros_impacto : Color
 @export var efectos_al_recibir_impacto : Array[EfectosOvillo]
